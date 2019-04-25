b9b38c7a96fab78ff26c6e966d7e7c9cb08ff36c
recitogigih <recitogigih9@gmail.com>
2019-04-25 23:07:28 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <input type="number" v-on:keyup.enter="getmodulus()" v-model="dataLength">
+    <ul>
+      <li v-for="(data, index) in dataLengths" :key="index">{{data}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  // for (i = 1; i <= this.dataLength; i++) {
+  //   if (i % 3 == 0) {
+  //      console.log(i + 'hello')
+  //   } else if (i % 5 == 0) {
+  //      console.log(i + 'world')
+  //   }
+  //   console.log(i)
+  // }
+
+
+
+
+  export default {
+    mounted() {
+      console.log((1, 3));
+    },
+
+    data() {
+      return {
+        dataLengths: []
+      }
+    },
+    methods: {
+      getmodulus() {
+        var i = 1;
+        var num = this.dataLength;
+        this.dataLengths = [];
+        for (i = 1; i <= num; i++) {          
+          if (i % 3 == 0) {
+            this.dataLengths.push(i + ' hello')
+          //  console.log(i + ' hello')  
+          } else if (i % 5 == 0) {
+             this.dataLengths.push(i + 'world')
+            // console.log(i + ' world')
+          } else {
+            this.dataLengths.push(i)
+            // console.log(i)
+          }
+        }
+        
+      }
+    }
+  }
+
+</script>
+
+
+<style>
+
+</style>
---
+++ pages/tes.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+       <!-- <input type="number" v-on:keyup.enter=""> -->
+      <nuxt-link to="/"> hgjj</nuxt-link>
+   </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
